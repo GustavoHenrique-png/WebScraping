2a3d2cd71830de6e0bd45168765e55febecbc597
Gustavo Henrique <ymooncake@gmail.com>
2022-06-15 15:28:29 -0300

    Update getHtml.py

--- project/getHtml.py
+++ project/getHtml.py
@@ -7,16 +7,8 @@ response = requests.get('https://www.supermercadoriodaspedras.com.br')
 
 content = response.content
 
-site = BeautifulSoup(content,'html.parser')
+site = BeautifulSoup(content, 'html.parser')
 
-#news = site.find('div', attrs={'class': 'feed-post-body'})
+news = site.find('div', attrs={'class': 'feed-post-body'})
 
 print(site.prettify)
-
-class getHtml():
-
-    response = requests.get('{site}')
-
-rioDasPedras = getHtml()
-
-rioDasPedras.response()
