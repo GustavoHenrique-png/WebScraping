c5a4e0e3f29fa55ecc46cf08c6dbf3d932578b05
Gustavo Henrique <ymooncake@gmail.com>
2022-06-21 14:40:20 -0300

    Automação e dataframe

--- project/getHtml.py
+++ project/getHtml.py
@@ -1,7 +1,10 @@
 import re
 from urllib import response
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
+
+Newslist = []
 
 # pegando site pela url
 response = requests.get('https://g1.globo.com')
@@ -12,11 +15,37 @@ content = response.content
 # Dando ao bs4 a indicação de qu eé um html
 site = BeautifulSoup(content, 'html.parser')
 
-# Usando find para procurar uma div no site
-news = site.find('div', attrs={'class': 'feed-post-body'})
+# Usando find para procurar uma div no site(metodo findAll para buscar todas as tags que possuem as mesmas especificações do parametros)
+news = site.findAll('div', attrs={'class': 'feed-post-body'})
 
-#Buscando mais especificamente dentro da notici
-titulo = news.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-color-hover'})
+#laço percorrendo as noticias do site
+for new in news:
+
+    #busca do titulo da noticia oqual contem o link
+    titulo = new.find('a', attrs={'class':'feed-post-link gui-color-primary gui-color-hover'})
+    
+    #Adicionando as noticias que tem titulo dentro de uma lista
+    if(titulo):
+        #lista de listas com titulo da materia e link da mesma
+        Newslist.append([titulo.text,titulo['href']])
+
+
+
+    #print(titulo.text,'\n')
+
+#colocando as listas numa tabela com o pandas
+Linkednews = pd.DataFrame(Newslist,columns=['titulo', 'link'])
+
+#criando arquivo csv com a tabela do pandas
+Linkednews.to_csv('noticiasGlobo.csv',index=False)
+
+#imprimindo a tabela do pandas
+print(Linkednews)
+
+#print(news)
+
+#Buscando mais especificamente dentro da noticia
+#titulo = news.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-color-hover'})
 
 # Pretify pra imprimir formatadinho
 # print(site.prettify())
@@ -25,4 +54,5 @@ titulo = news.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-co
 #print(news.pretify())
 
 #titulo ad noticia
-print(titulo.text)
+#print(titulo.text)
+
